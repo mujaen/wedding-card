35dbd4da0c46c23d48e34ac5cb13c1852cad81d9
mujaen <mujaen@nate.com>
2025-03-12 14:16:32 +0900

    Feat: 엘리먼트 fade 이벤트 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
-import React from "react";
-
-import logo from "./logo.svg";
+import React, { useEffect } from "react";
 
 function App() {
-  return <div className="flex h-screen w-full bg-gray-500">123</div>;
+  const options = {
+    root: null, // viewport
+    rootMargin: "20px",
+    threshold: 0.5, // 50%가 viewport에 들어와 있어야 callback 실행
+  };
+
+  const observer = new IntersectionObserver((entries) => {
+    console.log("여기");
+    entries.forEach((entry) => {
+      if (entry.intersectionRatio > 0) {
+        entry.target.classList.add("inView");
+      } else {
+        entry.target.classList.remove("inView");
+      }
+    });
+  }, options);
+
+  useEffect(() => {
+    const boxList = document.querySelectorAll(".title");
+
+    boxList.forEach((el) => observer.observe(el));
+  }, []);
+
+  return (
+    <div className="bg-gray-500">
+      <div className="title"></div>
+      <div className="title"></div>
+      <div className="title"></div>
+      <div className="title"></div>
+      <div className="title"></div>
+      <div className="title"></div>
+      <div className="title"></div>
+      <div className="title"></div>
+      <div className="title"></div>
+      <div className="title"></div>
+      <div className="title"></div>
+      <div className="title"></div>
+      <div className="title"></div>
+    </div>
+  );
 }
 
 export default App;
