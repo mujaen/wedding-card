75aa1618c7f5d9177fec848e12f32fd54b595c94
mujaen <mujaen@nate.com>
2025-08-25 21:10:44 +0900

    fix: 영상 추가

--- src/Video.js
+++ src/Video.js
@@ -1,25 +1,29 @@
-import React from "react";
+import React, { useRef } from "react";
 
-const videoSrc = "/assets/sample.mp4";
+const videoSrc = "/assets/sample2.mp4";
 
-const VideoPlayer = () => (
-  <video
-    src={videoSrc}
-    autoPlay
-    muted
-    playsInline
-    onEnded={() => {
-      if (videoRef.current) {
-        videoRef.current.pause();
-      }
-    }}
-    style={{
-      width: "100%",
-      height: "auto",
-      display: "block",
-      objectFit: "cover",
-    }}
-  />
-);
+const VideoPlayer = () => {
+  const videoRef = useRef(null);
+
+  return (
+    <video
+      src={videoSrc}
+      autoPlay
+      muted
+      playsInline
+      onEnded={() => {
+        if (videoRef.current) {
+          videoRef.current.pause();
+        }
+      }}
+      style={{
+        width: "100%",
+        height: "auto",
+        display: "block",
+        objectFit: "cover",
+      }}
+    />
+  );
+};
 
 export default React.memo(VideoPlayer);
